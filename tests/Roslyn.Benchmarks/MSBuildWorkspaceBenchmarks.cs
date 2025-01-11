822e9e28aeb7d8c82cdf9cbb3bf4ebb64cbc67fe
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2025-01-11 13:29:55 +1000

    add test and results

--- tests/Roslyn.Benchmarks/MSBuildWorkspaceBenchmarks.cs
+++ tests/Roslyn.Benchmarks/MSBuildWorkspaceBenchmarks.cs
@@ -8,12 +8,35 @@ namespace Roslyn.Benchmarks;
 public class MSBuildWorkspaceBenchmarks
 {
 	private const string _solutionFilePath = "C:/Users/Matthew/Documents/Git/StatusApp/StatusApp.sln";
-
+	
+	// | Method                    | Mean     | Error    | StdDev   |
+	// |-------------------------- |---------:|---------:|---------:|
+	// | CreateWorkspaceNoParams   | 10.88 us | 0.045 us | 0.042 us |
+	// | CreateWorkspaceWithParams | 11.22 us | 0.072 us | 0.060 us |
 	[Benchmark]
-	public async Task<Solution> ParseSolutionFileFromPath()
+	public MSBuildWorkspace CreateWorkspaceNoParams()
 	{
 		var workspace = MSBuildWorkspace.Create();
-		var solution = await workspace.OpenSolutionAsync(_solutionFilePath);
-		return solution;
+		return workspace;
 	}
+
+	[Benchmark]
+	public MSBuildWorkspace CreateWorkspaceWithParams()
+	{
+		var properties = new Dictionary<string, string>
+		{
+			{ "Configuration", "Debug" },
+			{ "Platform", "AnyCPU" }
+		};
+		var workspace = MSBuildWorkspace.Create(properties);
+		return workspace;
+	}
+
+	// [Benchmark]
+	// public async Task<Solution> ParseSolutionFileFromPath()
+	// {
+	// 	var workspace = MSBuildWorkspace.Create();
+	// 	var solution = await workspace.OpenSolutionAsync(_solutionFilePath);
+	// 	return solution;
+	// }
 }
