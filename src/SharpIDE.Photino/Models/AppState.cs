79a0b9edfbd63962b31325805e19ea270c81031d
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-08-02 16:30:13 +1000

    add setting

--- src/SharpIDE.Photino/Models/AppState.cs
+++ src/SharpIDE.Photino/Models/AppState.cs
@@ -2,11 +2,12 @@ namespace SharpIDE.Photino.Models;
 
 public class AppState
 {
-	public required string SolutionFilePath { get; set; }
+	public required string? SolutionFilePath { get; set; }
 	public required IdeSettings IdeSettings { get; set; } = new IdeSettings();
 }
 
 public class IdeSettings
 {
 	public bool AutoOpenLastSolution { get; set; }
+	public bool AutoOpenTerminalOnLaunch { get; set; }
 }
