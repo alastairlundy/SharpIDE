14db5bc3a17c4492533d81f24fa684aae0de72d6
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-09-25 00:30:50 +1000

    open file from search result

--- src/SharpIDE.Godot/Features/Search/SearchResultComponent.cs
+++ src/SharpIDE.Godot/Features/Search/SearchResultComponent.cs
@@ -8,17 +8,25 @@ public partial class SearchResultComponent : MarginContainer
     private Label _matchingLineLabel = null!;
     private Label _fileNameLabel = null!;
     private Label _lineNumberLabel = null!;
+    private Button _button = null!;
     
     public SearchResult Result { get; set; } = null!;
     
     public override void _Ready()
     {
+        _button = GetNode<Button>("Button");
         _matchingLineLabel = GetNode<Label>("%MatchingLineLabel");
         _fileNameLabel = GetNode<Label>("%FileNameLabel");
         _lineNumberLabel = GetNode<Label>("%LineNumberLabel");
         SetValue(Result);
+        _button.Pressed += OnButtonPressed;
     }
-    
+
+    private void OnButtonPressed()
+    {
+        GodotGlobalEvents.InvokeFileExternallySelected(Result.File);
+    }
+
     private void SetValue(SearchResult result)
     {
         if (result is null) return;
