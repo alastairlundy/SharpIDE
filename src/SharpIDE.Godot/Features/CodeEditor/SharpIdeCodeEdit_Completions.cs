77160b60025d01673a1fb2feb9f2f91cae07a27e
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-10-27 21:45:22 +1000

    add namespace icon

--- src/SharpIDE.Godot/Features/CodeEditor/SharpIdeCodeEdit_Completions.cs
+++ src/SharpIDE.Godot/Features/CodeEditor/SharpIdeCodeEdit_Completions.cs
@@ -12,7 +12,7 @@ public partial class SharpIdeCodeEdit
     private readonly Texture2D _fieldIcon = ResourceLoader.Load<Texture2D>("uid://c4y7d5m4upfju");
     private readonly Texture2D _propertyIcon = ResourceLoader.Load<Texture2D>("uid://y5pwrwwrjqmc");
     private readonly Texture2D _keywordIcon = ResourceLoader.Load<Texture2D>("uid://b0ujhoq2xg2v0");
-    // namespace
+    private readonly Texture2D _namespaceIcon = ResourceLoader.Load<Texture2D>("uid://bob5blfjll4h3");
 
     private Texture2D? GetIconForCompletion(SymbolKind? symbolKind, TypeKind? typeKind, Accessibility? accessibility, bool isKeyword)
     {
@@ -25,6 +25,7 @@ public partial class SharpIdeCodeEdit
             (SymbolKind.Local, _, _) => _localVariableIcon,
             (SymbolKind.Field, _, _) => _fieldIcon,
             (SymbolKind.Property, _, _) => _propertyIcon,
+            (SymbolKind.Namespace, _, _) => _namespaceIcon,
             _ => null
         };    
         return texture;
